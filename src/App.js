1e65698c6b4c8454cb40e038f1c37d3beff72381
brutalvik <vikcanada90@gmail.com>
2023-12-24 19:25:05 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense } from "react";
+import styles from "App.module.css";
+import { Routes, Route } from "react-router-dom";
+import { Spinner } from "@chakra-ui/react";
 
-function App() {
+const Home = lazy(() => import("components/Home/home"));
+const Dashboard = lazy(() => import("components/Dashboard/Dashboard"));
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense
+      fallback={
+        <Spinner
+          thickness="10px"
+          speed="0.50s"
+          emptyColor="teal.200"
+          color="blue.500"
+          size="xl"
+        />
+      }
+    >
+      <div className={styles.App}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Suspense>
   );
-}
+};
 
 export default App;
